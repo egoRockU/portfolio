17042fb69546983b348713ed11b81e53e97f1dd8
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2024-04-05 21:17:16 +0800

    added skills

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, json, url_for
+from flask.views import View
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/skills")
+def skills():
+    file = os.path.join(app.static_folder, 'data', 'skills.json')
+    with open(file) as test_file:
+        data = json.load(test_file)
+    obj = {
+        'title': "Skills",
+        'skills': data
+    }
+    print(obj)
+    return render_template("skills.html", obj=obj)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
