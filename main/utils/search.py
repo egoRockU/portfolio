cfbf4d0ba4216f7667c1061739c4de0247226b94
egoRockU <gerwin.lagutan.dev@gmail.com>
2025-04-15 14:26:56 +0800

    fixed bugs on search

--- main/utils/search.py
+++ main/utils/search.py
@@ -6,6 +6,11 @@ def search(projects, entry):
                 result[project] = attrib
             else:
                 for key, value in attrib.items():
-                    if value.lower().find(entry.lower()) != -1:
-                        result[project] = attrib
+                    if isinstance(value, str):
+                        if value.lower().find(entry.lower()) != -1:
+                            result[project] = attrib
+                    elif isinstance(value, list):
+                        for item in value:
+                            if item.lower().find(entry.lower()) != -1:
+                                result[project] = attrib
     return result
